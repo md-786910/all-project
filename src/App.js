0701d89e7fb916f6977c427b57aa9a5443d01304
md ashif reza <mdashifreza111@gmail.com>
2021-05-06 12:23:52 +0530

    my todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Button from '@material-ui/core/Button';
+const App = () => {
+    const [set, setext] = useState('');
+    const [item, setitem] = useState([]);
+
+    const textchange = (e) => {
+        setext(e.target.value);
+    }
+
+    const submit = () => {
+        if (set == '') {
+            alert("Write...");
+        }
+        else {
+            setitem([...item, set]);
+
+            localStorage.setItem();
+            setext('');
+        }
+
+
+    }
+
+    // delete item
+    const Delete = (id) => {
+        const newList = item.filter((element, ind) => ind != id);
+
+        setitem(newList);
+
+    }
+    return (
+        <>
+            <div id="container">
+                <div id="heading">
+                    <h2> Todo App</h2>
+                </div>
+                <div id="inputBox">
+                    <input type="text" name="text" id="text" value={set} onChange={textchange} placeholder="Write..." />
+
+                    <Button color="black" variant="contained" onClick={submit}><AddIcon></AddIcon></Button>
+                </div>
+            </div>
+            {/* <Table /> */}
+            <div id="table">
+                <h1>Your Items</h1>
+                <div id="content">
+                    <table className="table  table-striped">
+                        <thead>
+                            <tr>
+                                <th>S.NO</th>
+                                <th>Todo Items</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+
+                            {
+                                item.map((element, index) => {
+
+                                    return (
+
+                                        < tr >
+                                            <td>{index + 1}</td>
+                                            <td>{element}</td>
+                                            <td><Button onClick={() => Delete(index)}><DeleteIcon></DeleteIcon></Button></td>
+                                        </tr >
+                                    )
+                                })
+
+                            }
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App;
